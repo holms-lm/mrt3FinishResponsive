426a2fa03e945b147cb94b69b6d868494ae24ea7
holms <serg882244@gmail.com>
2019-04-28 19:14:54 +0300

    initial

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var terser = require('gulp-terser');
+
+sass.compiler = require('node-sass');
+
+gulp.task('style', function () {
+    return gulp.src('./src/main.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./src/**/*.scss',  gulp.parallel('style'));
+    gulp.watch('./src/**/*.js',  gulp.parallel('style'));
+});
+
+gulp.task('script', function () {
+    return gulp.src('./src/main.js')
+        .pipe(terser())
+        .pipe(gulp.dest('./dist'));
+});
